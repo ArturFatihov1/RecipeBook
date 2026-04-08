921a63fcb202b357e0a2b62666d9ee936d16f989
Vladimir <artifactorymail@gmail.com>
2026-04-08 23:17:29 +0300

    RB-18 RecipeListXml

--- app/src/androidTest/java/com/example/recipebook/recipelist/LoadingDialogUi.kt
+++ app/src/androidTest/java/com/example/recipebook/recipelist/LoadingDialogUi.kt
@@ -1,31 +1,27 @@
 package com.example.recipebook.recipelist
 
-import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.example.recipebook.R
 import com.example.recipebook.core.AbstractUi
-import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
 
-class LoadingDialogUi(
-    containerIdMatcher: Matcher<View>,
-    classTypeMatcher: Matcher<View>
-) : AbstractUi(
+class LoadingDialogUi : AbstractUi(
     interaction = onView(
         allOf(
-            containerIdMatcher,
-            classTypeMatcher,
             withId(R.id.loadingDialogView),
+            isAssignableFrom(ConstraintLayout::class.java),
             hasDescendant(
                 allOf(
                     withId(R.id.loadingTitle),
-                    withText(R.string.loadingTitle),
+                    withText(R.string.loading_title),
                     isAssignableFrom(TextView::class.java)
                 )
             ),
@@ -33,12 +29,17 @@ class LoadingDialogUi(
             hasDescendant(
                 allOf(
                     withId(R.id.loadingBody),
-                    withText(R.string.loadingBody),
+                    withText(R.string.loading_body),
                     isAssignableFrom(TextView::class.java)
                 )
             ),
 
-            hasDescendant(isAssignableFrom(ProgressBar::class.java))
+            hasDescendant(
+                allOf(
+                    withId(R.id.loadingProgress),
+                    isAssignableFrom(ProgressBar::class.java)
+                )
+            )
         )
     ).inRoot(isDialog())
 )
