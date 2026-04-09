4c9320ee430beec5376c45f41874b8dd04b1306d
ukiD0 <valentinaaakkk@yandex.ru>
2026-04-09 22:44:52 +0300

    RB-19 FavoriteViewModel

---
+++ app/src/main/java/com/example/recipebook/favorite/presentation/FavoriteUiObservable.kt
@@ -0,0 +1,6 @@
+package com.example.recipebook.favorite.presentation
+
+import com.example.recipebook.detail.core.UiObservable
+import com.example.recipebook.favorite.FavoriteUiState
+
+interface FavoriteUiObservable : UiObservable<FavoriteUiState>
